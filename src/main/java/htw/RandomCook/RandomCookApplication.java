376430b1118f008a8b42d0143299bcedf3a2211e
Goekalp <Goekalpyildiz@outlook.de>
2021-05-12 11:03:29 +0200

    Spring.io generierte Daten hinzugefügt

---
+++ src/main/java/htw/RandomCook/RandomCookApplication.java
@@ -0,0 +1,13 @@
+package htw.RandomCook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomCookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomCookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/htw/RandomCook/RandomCookApplicationTests.java
@@ -0,0 +1,13 @@
+package htw.RandomCook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomCookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
